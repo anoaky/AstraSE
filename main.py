276af5b7431a8c9afc86310a78e4200de1ea36b0
1strays <7695591+1strays@users.noreply.github.com>
2024-07-08 19:00:05 -0400

    refactored the way groups are added to the bot

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ import discord
 import asyncio
 import astra
 
+async def add_groups(bot):
+    groups = [
+        astra.groups.DebugGroup(bot),
+        astra.groups.QuoteGroup(bot)
+    ]
+    async with asyncio.TaskGroup() as tg:
+        for grp in groups:
+            tg.create_task(bot.add_cog(grp))
+
 if __name__ == '__main__':
     token = os.environ.get('TOKEN')
     intents = discord.Intents.default()
     intents.message_content = True
     bot = astra.Astra('!', intents=intents)
-    asyncio.run(bot.add_cog(astra.groups.DebugGroup(bot)))
-    asyncio.run(bot.add_cog(astra.groups.QuoteGroup(bot)))
+    asyncio.run(add_groups(bot))
     bot.run(token)
