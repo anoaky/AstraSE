92df5dc44fb753b46581db28626d91dfc0f693c5
1strays <7695591+1strays@users.noreply.github.com>
2024-07-18 16:32:48 -0400

    tossed the `info` command because it never seemed to work right

--- src/astra/astra.py
+++ src/astra/astra.py
@@ -6,23 +6,7 @@ import sys
 from astra.connections import AstraDBConnection, AstraHandler
 from astra.groups import *
 
-info_text = """
-**ASTRA: SE**
-A Simple Text Recording Assistant: Special Edition v1.3.3
-
-**New features:**
-- Changed the reply to `/quote add` to include the ID of the new quote."""
-
 class Astra(commands.Bot):
-    def __init__(self, prefix, **kwargs):
-        super().__init__(prefix, **kwargs)
-        base_cmds = [
-            app_commands.Command(name='info',
-                                 description='Version, changelog, and other info',
-                                 callback=self.info)
-        ]
-        for cmd in base_cmds:
-            self.tree.add_command(cmd)
         
     async def setup_hook(self) -> None:
         await super().setup_hook()
@@ -37,6 +21,3 @@ class Astra(commands.Bot):
             return
         else:
             await AstraHandler.check_profanity(message)
-        
-    async def info(self, interaction: discord.Interaction):
-        await interaction.response.send_message(info_text, ephemeral=interaction.user.guild_permissions.administrator)
